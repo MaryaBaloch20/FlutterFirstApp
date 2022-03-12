ca30ab10a55b12ad180520a9cc22cd3d02df3d60
Marya Baloch <maryabaloch7@gmail.com>
2022-03-12 18:24:02 +0500

    Grid View HALF DONE

--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:firt_app/models/catalog.dart';
 import 'package:firt_app/widgets/drawer.dart';
@@ -25,7 +26,8 @@ class _HomePageState extends State<HomePage> {
     var catalogJson = await rootBundle.loadString("assets/Files/catalog.json");
     var decodedData = jsonDecode(catalogJson);
     var productData = decodedData["products"];
-    CatalogModel.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
@@ -42,14 +44,44 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Catalog App"),
         centerTitle: true,
       ),
-      body: ListView.builder(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+        ),
+        itemBuilder: (context, index) {
+          final item = CatalogModel.items[index];
+          return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                header: Container(
+                  child: Text(item.name,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      )),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.blue[900],
+                  ),
+                ),
+                child: Image.network(item.imageUrl),
+                footer: Text(item.price.toString()),
+              ));
+        },
         itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) =>
-           ItemWidget(
-            item: CatalogModel.items[index],
-          )
-        
       ),
+      //Can be done with list view Like this:
+      // ListView.builder(
+      //   itemCount: CatalogModel.items.length,
+      //   itemBuilder: (context, index) =>
+      //      ItemWidget(
+      //       item: CatalogModel.items[index],
+      //     )
+
+      // ),
       drawer: const MyDrawer(),
     );
   }
